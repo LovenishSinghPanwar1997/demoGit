fda28c4b4b31d1ca68d52abbff5491ea9663ea66
LovenishSinghPanwar1997 <lovenish.singh@coviam.com>
2021-02-16 14:18:39 +0530

    testing

--- src/main/java/com/demo/demoGit/DemoGitApplication.java
+++ src/main/java/com/demo/demoGit/DemoGitApplication.java
@@ -9,6 +9,7 @@ public class DemoGitApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoGitApplication.class, args);
 		DemoGit demoGit = new DemoGit();
+		DemoGit demoGit1 = new DemoGit();
 	}
 
 }

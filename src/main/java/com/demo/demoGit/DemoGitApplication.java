b08fb6bc4dffe56db429d0b2f13e0f7843c81db2
LovenishSinghPanwar1997 <lovenish.singh@coviam.com>
2021-02-16 17:49:12 +0530

    changes

--- src/main/java/com/demo/demoGit/DemoGitApplication.java
+++ src/main/java/com/demo/demoGit/DemoGitApplication.java
@@ -9,6 +9,8 @@ public class DemoGitApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoGitApplication.class, args);
 		DemoGit demoGit = new DemoGit();
+		//trying to create a conflict again
+		DemoGit demoGit2 = new DemoGit();
 		DemoGit demoGit1 = new DemoGit();
 		//i am trying to create a merge conflict
 		System.out.println("Hello there!");
